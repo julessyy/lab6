51c7b1b2d5f565f109508adb5a96624cd7bafbeb
Julianne S <sgoifoj1@owls.southernct.edu>
2015-06-22 14:38:18 -0400

    init

---
+++ gender.py
@@ -0,0 +1,25 @@
+
+from flask import Flask,render_template
+#import simplejson 
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def greetings():
+     return 'Hello there!'
+
+@app.route('/gender')
+def gender():
+
+   return render_template("schools.html") 
+
+@app.route('/native_american')
+def native_american():
+    return render_template("indian.html")
+
+if __name__ == '__main__':
+    app.run(port=5002, #host='127.0.0.1',
+            debug=True)
+
+	
