001bcd200af78e886383b3c572ce5dd55a556886
julessyy <julesgoifo@gmail.com>
2015-06-23 12:45:32 -0400

    Create bgscript.py

---
+++ app/bgscript.py
@@ -0,0 +1,25 @@
+
+from flask import Flask,render_template
+from flask.ext.script import Manager
+
+app = Flask(__name__)
+
+manager = Manager(app)
+
+@app.route('/')
+def main():
+     return 'Hello there!'
+
+@app.route('/gender')
+def gender():
+
+   return render_template("schools.html") 
+
+@app.route('/native_american')
+def native_american():
+    return render_template("indian.html")
+
+if __name__ == '__main__':
+    manager.run()
+
+	
